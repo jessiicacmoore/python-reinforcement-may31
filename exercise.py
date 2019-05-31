db17c9459bc9cfa61186ec130e8b5eafa34dc25e
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-31 10:05:41 -0400

    Pasted in ballots dictionary

---
+++ exercise.py
@@ -0,0 +1,8 @@
+ballots = [
+    {"gold": "Smudge", "silver": "Tigger", "bronze": "Simba"},
+    {"gold": "Bella", "silver": "Lucky", "bronze": "Tigger"},
+    {"gold": "Bella", "silver": "Boots", "bronze": "Smudge"},
+    {"gold": "Boots", "silver": "Felix", "bronze": "Bella"},
+    {"gold": "Lucky", "silver": "Felix", "bronze": "Bella"},
+    {"gold": "Smudge", "silver": "Simba", "bronze": "Felix"},
+]
